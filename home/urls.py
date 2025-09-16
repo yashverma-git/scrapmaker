5e427f05c71be5e38e0b18399c83ef6e35e0d2f3
Yash verma <yashverma7417@gmail.com>
2025-09-16 19:41:50 +0530

    added the dropdown menu pages

--- home/urls.py
+++ home/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('features/', views.features, name='features'),
-    path('buy/', views.buy, name='buy'),
-    path('sell/', views.sell, name='sell'),
-    path('offer/', views.offer, name='offer'),
+    path('', views.index, name='home'),          # Home page
+    path('about/', views.about, name='about'),   # About page
+    path('contact/', views.contact, name='contact'), # Contact page
+    path('features/', views.features, name='features'), # Features page
+    path('buy/', views.buy, name='buy'),          # Buy page
+    path('sell/', views.sell, name='sell'),        # Sell page
+    path('offer/', views.offer, name='offer'),   # Offer page
 ]
